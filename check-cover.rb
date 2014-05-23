bc16d1e85d61a6cfbc791126d3f2c0ff010d91fa
John McKerrell <john@mckerrell.net>
2014-05-23 12:16:36 +0100

    Script to check that we have cover for a date.

---
+++ check-cover.rb
@@ -0,0 +1,78 @@
+#!/usr/bin/ruby
+# check-cover.rb
+# Script to check that the doodle poll to
+# see if we have cover for tomorrow (/ any date).
+
+require 'rubygems'
+require 'pp'
+require 'uri'
+require 'net/http'
+require 'time'
+require 'json'
+require_relative 'time_start_and_end_extensions'
+require_relative 'local_config'
+
+def usage
+  puts "usage: check-cover.rb <doodle-url> [<YYYY-MM-DD>]"
+end
+
+if ARGV.length < 1
+  usage
+  exit
+end
+
+poll = ARGV.shift
+check_date = ARGV.shift
+
+if check_date.nil?
+  check_date = (Time.now + 1.day).strftime("%Y-%m-%d")
+end
+
+response = Net::HTTP.get_response(URI.parse(poll))
+
+json = response.body
+json = json.gsub(/.*doodleJS.data.poll =/m, '')
+json = json.gsub(/;\n.*/m, '')
+
+poll = JSON.parse(json)
+
+raise "Couldn't parse json" if json.nil?
+
+options = poll["fcOptions"]
+people = poll["participants"]
+
+raise "Couldn't find options" if options.nil?
+raise "Couldn't find people" if people.nil?
+
+
+options.each do |option|
+  option[:definites] = []
+  option[:maybes] = []
+end
+
+people.each do |person|
+  person["preferences"].split("").each_index do |i|
+    pref = person["preferences"][i]
+    if pref == 'y'
+      options[i][:definites] << person["name"]
+    elsif pref == 'i'
+      options[i][:maybes] << person["name"]
+    end
+  end
+end
+
+lacking = []
+options.each do |option|
+  day = Time.at(option["start"]).strftime("%Y-%m-%d")
+  if day == check_date and option[:definites].length == 0
+    option["formatted_date"] = day
+    lacking << option
+  end
+end
+
+if lacking.length > 0
+  puts "We are missing support for the following time periods:"
+  lacking.each do |option|
+    puts "    #{option["formatted_date"]} #{option["text"]} #{option[:maybes].length == 0 ? "(With no maybes)" : "(With these maybes: #{option[:maybes].join(", ")})" }"
+  end
+end
